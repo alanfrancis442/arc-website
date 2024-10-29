50a96c9d2ebbdd2c3c1c50496df60424b9bcb89d
Alan Francis <alan.prime567@gmail.com>
2024-10-30 00:33:55 +0530

    fix:word animation

--- src/sections/landing.tsx
+++ src/sections/landing.tsx
@@ -1,6 +1,5 @@
 import Nav from "../components/nav";
 import { useRef } from "react";
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import useThemeStore from "../hooks/themeStore";
 import { FaDiscord, FaInstagram, FaLinkedin, FaTelegram } from "react-icons/fa";
@@ -8,35 +7,48 @@ import { FaXTwitter } from "react-icons/fa6";
 import Frame from "../components/frame";
 import { ImRocket } from "react-icons/im";
 import SmallFrame from "../components/smallFrame";
+import { useGSAP } from "@gsap/react";
 
 const Landing = () => {
   const words = ["Play.", "Reality.", "Gaming."];
-  // const [isDark, setIsDarkMode] = useState(false);
   const { isDark, setTheme } = useThemeStore();
-
   const container = useRef<HTMLDivElement>(null);
+  const textContainer = useRef<HTMLDivElement>(null);
 
   useGSAP(
     () => {
-      const tl = gsap.timeline({ repeat: -1, duration: 2 });
-      tl.to(".anitext0", {
-        y: "-120%",
-      })
-        .to(".anitext1", {
-          y: "0%",
-        })
-        .to(".anitext1", {
-          y: "-100%",
-        })
-        .to(".anitext2", {
-          y: "0%",
-        })
-        .to(".anitext2", {
-          y: "-100%",
+      const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.5 });
+
+      words.forEach((word, wordIndex) => {
+        const letters = word.split("");
+        letters.forEach((_, letterIndex) => {
+          tl.fromTo(
+            `.anitext${wordIndex} .letter${letterIndex}`,
+            { y: 50 },
+            {
+              y: wordIndex === 0 ? 0 : `${-100 * wordIndex}%`,
+
+              duration: 0.5,
+              ease: "power2.inOut",
+              delay: letterIndex * 0.05,
+            },
+            wordIndex * 1.5
+          ).to(
+            `.anitext${wordIndex} .letter${letterIndex}`,
+            {
+              y: -100 * (wordIndex + 0.65),
+
+              duration: 0.5,
+              ease: "power2.inOut",
+              delay: letterIndex * 0.05,
+            },
+            wordIndex * 1.5 + 1.5
+          );
         });
+      });
     },
     {
-      scope: container,
+      scope: textContainer,
     }
   );
 
@@ -70,16 +82,28 @@ const Landing = () => {
         <div className="text-3xl font-bold absolute z-10 top-1/2 -translate-y-[50%] md:left-[15%] left-5 ">
           <div className="overflow-clip relative flex md:mb-4">
             <span className="font-[gilmer] md:text-6xl text-2xl">Redefine</span>{" "}
-            <div className="relative md:h-16 h-8 ml-1">
-              {words.map((word, index) => (
+            <div
+              ref={textContainer}
+              style={{
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+              }}
+              className="relative md:h-16 h-8  ml-1"
+            >
+              {words.map((word, wordIndex) => (
                 <div
-                  key={index}
-                  className={`font-[gilmer] md:text-6xl text-2xl anitext${index}`}
-                  style={{
-                    transform: `translateY(${index * 100}%)`,
-                  }}
+                  key={wordIndex}
+                  className={`font-[gilmer] md:text-6xl text-2xl anitext${wordIndex}`}
                 >
-                  {word}
+                  {word.split("").map((letter, letterIndex) => (
+                    <span
+                      key={letterIndex}
+                      className={`inline-block letter${letterIndex} ${
+                        isDark ? "text-[#FEFEFE]" : "text-[#252525]"
+                      }`}
+                    >
+                      {letter}
+                    </span>
+                  ))}
                 </div>
               ))}
             </div>
@@ -94,14 +118,13 @@ const Landing = () => {
             font-normal flex justify-center items-center md:gap-5 gap-3 max-sm:text-sm `}
             >
               launching soon <ImRocket />
-              {/* <MdOutlineArrowOutward className="rotate-90 md:text-5xl font-mono " /> */}
             </button>
           </SmallFrame>
         </div>
       </div>
 
       <div
-        className="absolute flex flex-col md:right-24 right-0    top-0 h-full text-[10rem] md:text-[14rem] font-[sentex] -translate-y-[8%] leading-tight "
+        className="absolute flex flex-col md:right-24 right-0 top-0 h-full text-[10rem] md:text-[14rem] font-[sentex] -translate-y-[8%] leading-tight "
         style={{
           WebkitTextStroke: isDark ? "1px #ffffff" : "2px #000000",
           color: isDark ? "#252525" : "#FFFFFF",
@@ -124,25 +147,23 @@ const Landing = () => {
         src="/stripe2.svg"
         alt=""
       />
-      {
-        <div className="absolute bottom-10 md:left-10 left-8 flex md:gap-8 gap-3">
-          <a href="#">
-            <FaLinkedin className="text-xl md:text-2xl" />
-          </a>
-          <a href="#">
-            <FaXTwitter className="text-xl md:text-2xl" />
-          </a>
-          <a href="#">
-            <FaInstagram className="text-xl md:text-2xl" />
-          </a>
-          <a href="#">
-            <FaDiscord className="text-xl md:text-2xl" />
-          </a>
-          <a href="#">
-            <FaTelegram className="text-xl md:text-2xl" />
-          </a>
-        </div>
-      }
+      <div className="absolute bottom-10 md:left-10 left-8 flex md:gap-8 gap-3">
+        <a href="#">
+          <FaLinkedin className="text-xl md:text-2xl" />
+        </a>
+        <a href="#">
+          <FaXTwitter className="text-xl md:text-2xl" />
+        </a>
+        <a href="#">
+          <FaInstagram className="text-xl md:text-2xl" />
+        </a>
+        <a href="#">
+          <FaDiscord className="text-xl md:text-2xl" />
+        </a>
+        <a href="#">
+          <FaTelegram className="text-xl md:text-2xl" />
+        </a>
+      </div>
     </div>
   );
 };
